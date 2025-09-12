81d14255a96c5c028ea4c306c8d5e9a80ed2b401
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-09-12 16:37:27 -0400

    Add code coverage exclusions for scaffolded models
    This commit introduces the `using System.Diagnostics.CodeAnalysis;` directive and the `[ExcludeFromCodeCoverage]` attribute to multiple classes in the `AdventureWorks.Models` namespace. These changes indicate that the affected classes, which are generated models for database scaffolding, should not be included in code coverage analysis.
    
    Additionally, the `#pragma warning disable CS1591` directive is added to suppress warnings about missing XML comments for publicly visible types or members. The updates encompass a wide range of classes, including `Address`, `AddressType`, `AdventureWorksDbContext`, `Customer`, `Employee`, and various view models, enhancing test coverage management and overall code quality.

--- extra-projects/AdventureWorks/Models/Password.cs
+++ extra-projects/AdventureWorks/Models/Password.cs
@@ -1,9 +1,12 @@
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+using System.Diagnostics.CodeAnalysis;
+
 namespace AdventureWorks.Models;
 
 /// <summary>
 /// One way hashed authentication information
 /// </summary>
+[ExcludeFromCodeCoverage(Justification = "These are test models created by scaffolding the database and should not be tested")]
 public partial class Password
 {
 	public int BusinessEntityId { get; set; }
--- extra-projects/AdventureWorks/Models/VStoreWithContact.cs
+++ extra-projects/AdventureWorks/Models/VStoreWithContact.cs
@@ -1,6 +1,9 @@
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+using System.Diagnostics.CodeAnalysis;
+
 namespace AdventureWorks.Models;
 
+[ExcludeFromCodeCoverage(Justification = "These are test models created by scaffolding the database and should not be tested")]
 public partial class VStoreWithContact
 {
 	public int BusinessEntityId { get; set; }
