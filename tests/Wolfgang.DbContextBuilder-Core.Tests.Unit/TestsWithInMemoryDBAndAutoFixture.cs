4a91c78df24c390917c96bd37c14d5c36e34e24a
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-09-15 14:23:54 -0400

    Enhance test classes with output helper and SQL commands
    Updated `DbContextBuilderTestsBase` and derived test classes to accept an `ITestOutputHelper` for improved test output management. Implemented `SelectTablesCommandText` property in each class, with specific SQL commands for SQLite and a `NotImplementedException` for others. Added comments to clarify the purpose of the new property.

--- tests/Wolfgang.DbContextBuilder-Core.Tests.Unit/TestsWithInMemoryDBAndAutoFixture.cs
+++ tests/Wolfgang.DbContextBuilder-Core.Tests.Unit/TestsWithInMemoryDBAndAutoFixture.cs
@@ -1,13 +1,15 @@
 using AdventureWorks.Models;
 using Microsoft.EntityFrameworkCore;
+using Xunit.Abstractions;
 
 namespace Wolfgang.DbContextBuilderCore.Tests.Unit;
 
 /// <summary>
 /// Runs all the tests using the default values for DbContextBuilder
 /// </summary>
-public class TestsWithInMemoryDBAndAutoFixture : DbContextBuilderTestsBase
+public class TestsWithInMemoryDbAndAutoFixture(ITestOutputHelper testOutputHelper) : DbContextBuilderTestsBase(testOutputHelper)
 {
+
     /// <summary>
     /// 
     /// </summary>
@@ -20,7 +22,16 @@ public class TestsWithInMemoryDBAndAutoFixture : DbContextBuilderTestsBase
     }
 
 
+    /// <summary>
+    /// The SQL command to select schema and table names from the database.
+    /// </summary>
+    /// <remarks>
+    /// If the database does not support schemas, like Sqlite, then return and empty string form the schema
+    /// </remarks>
+    protected override string SelectTablesCommandText => throw new NotImplementedException();
 
+
+    
     /// <summary>
     /// Verifies that UseInMemory returns a DbContext{T} for chaining additional calls
     /// </summary>
