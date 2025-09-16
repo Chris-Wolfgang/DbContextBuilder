89c24f0cfea9e4b84b44e7f23323c7a76badafbf
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-09-15 21:09:23 -0400

    Enhance AutoFixture integration and improve tests
    - Updated `AutoFixtureRandomEntityGenerator` to handle a customizable `Fixture`.
    - Added a new constructor for user-defined `Fixture` instances and included XML documentation.
    - Introduced `UseCustomRandomEntityGenerator` method in `DbContextBuilder<T>` for custom entity generation with null-checking.
    - Changed `IGenerateRandomEntities` interface from `internal` to `public` for better accessibility.
    - Added unit tests in `DbContextBuilderTestsBase` to verify new functionality and ensure consistency.
    - Refactored `SqliteModelCacheKeyFactory` and `SqliteModelCustomizer` for improved readability.
    - Added TODO comments for future work on Bogus integration and testing.

--- src/Wolfgang.DbContextBuilder-Core/global.cs
+++ src/Wolfgang.DbContextBuilder-Core/global.cs
@@ -1,3 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Wolfgang.DbContextBuilder-Core.Tests.Unit")]
+
+
+
+// TODO Create implementations for Bogus
+// TODO Allow user to configure Bogus
+// TODO Add tests
