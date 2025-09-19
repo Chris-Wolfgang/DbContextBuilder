a7408e12164ee5c2b9634b542e132f3252426e72
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-09-19 16:58:36 -0400

    Refactor models and tests for clarity and organization
    - Removed equality and hash code methods in Address.cs, StateProvince.cs, and updated comments.
    - Enhanced DatabaseLog.cs with XML comments and streamlined property definitions.
    - Improved formatting and readability in AutoFixtureRandomEntityGeneratorTests.cs.
    - Added namespace and adjusted warning handling in DbContextBuilderTestsBase.cs.
    - Simplified method definitions in TestsWithDefaults.cs and TestsWithSqliteAndAutoFixture.cs.
    - Updated Wolfgang.DbContextBuilder-Core.Tests.Unit.csproj for package versioning and organization.

--- tests/Wolfgang.DbContextBuilder-Core.Tests.Unit/AutoFixtureRandomEntityGeneratorTests.cs
+++ tests/Wolfgang.DbContextBuilder-Core.Tests.Unit/AutoFixtureRandomEntityGeneratorTests.cs
@@ -1,63 +1,62 @@
 ﻿using AutoFixture;
 
-namespace Wolfgang.DbContextBuilderCore.Tests.Unit
+namespace Wolfgang.DbContextBuilderCore.Tests.Unit;
+
+/// <summary>
+/// Tests for the AutoFixtureRandomEntityGenerator class
+/// </summary>
+public class AutoFixtureRandomEntityGeneratorTests : IGenerateRandomEntitiesTestsBase
 {
-    /// <summary>
-    /// Tests for the AutoFixtureRandomEntityGenerator class
-    /// </summary>
-    public class AutoFixtureRandomEntityGeneratorTests : IGenerateRandomEntitiesTestsBase
-    {
-        /// <summary>
-        /// Creates an instance of AutoFixtureRandomEntityGenerator to be tested.
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        protected override IGenerateRandomEntities CreateRandomEntityGenerator() => new AutoFixtureRandomEntityGenerator();
+	/// <summary>
+	/// Creates an instance of AutoFixtureRandomEntityGenerator to be tested.
+	/// </summary>
+	/// <returns></returns>
+	/// <exception cref="NotImplementedException"></exception>
+	protected override IGenerateRandomEntities CreateRandomEntityGenerator() => new AutoFixtureRandomEntityGenerator();
 
 
 
-        /// <summary>
-        /// Verifies that the Fixture property is not null
-        /// </summary>
-        [Fact]
-        public void Property_Fixture_is_not_null()
-        {
-            // Arrange
-            var sut = new AutoFixtureRandomEntityGenerator();
+	/// <summary>
+	/// Verifies that the Fixture property is not null
+	/// </summary>
+	[Fact]
+	public void Property_Fixture_is_not_null()
+	{
+		// Arrange
+		var sut = new AutoFixtureRandomEntityGenerator();
 
-            // Act & Assert
-            Assert.NotNull(sut.Fixture);
-        }
-
-
-
-        /// <summary>
-        /// Verifies that passing null to the constructor throws an ArgumentNullException
-        /// </summary>
-        [Fact]
-        public void Ctor_Fixture_when_passed_null_throws_ArgumentNullException()
-        {
-            // Act & Assert
-            var ex = Assert.Throws<ArgumentNullException>(() => new AutoFixtureRandomEntityGenerator(null!));
-            Assert.Equal("fixture", ex.ParamName);
-        }
+		// Act & Assert
+		Assert.NotNull(sut.Fixture);
+	}
 
 
 
-        /// <summary>
-        /// Verifies that the value passed into the constructor is assigned to the Fixture property
-        /// </summary>
-        [Fact]
-        public void Sets_Fixture_property_to_the_value_passed_into_ctor()
-        {
-            // Arrange
-            var fixture = new Fixture();
+	/// <summary>
+	/// Verifies that passing null to the constructor throws an ArgumentNullException
+	/// </summary>
+	[Fact]
+	public void Ctor_Fixture_when_passed_null_throws_ArgumentNullException()
+	{
+		// Act & Assert
+		var ex = Assert.Throws<ArgumentNullException>(() => new AutoFixtureRandomEntityGenerator(null!));
+		Assert.Equal("fixture", ex.ParamName);
+	}
 
-            // Act
-            var sut = new AutoFixtureRandomEntityGenerator(fixture);
 
-            // Assert
-            Assert.Equal(fixture, sut.Fixture);
-        }
-    }
+
+	/// <summary>
+	/// Verifies that the value passed into the constructor is assigned to the Fixture property
+	/// </summary>
+	[Fact]
+	public void Sets_Fixture_property_to_the_value_passed_into_ctor()
+	{
+		// Arrange
+		var fixture = new Fixture();
+
+		// Act
+		var sut = new AutoFixtureRandomEntityGenerator(fixture);
+
+		// Assert
+		Assert.Equal(fixture, sut.Fixture);
+	}
 }
