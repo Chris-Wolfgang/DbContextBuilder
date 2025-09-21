130c88937be6e1bca77e6d27cbb3d52e9c4b4fb6
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-09-21 13:25:56 -0400

    Code cleanup
    Removed commented-out code for `UseServiceProvider`, indicating a potential refactor. Modified `UseServices` to throw a `NotImplementedException`, marking it as pending implementation. Added a TODO for configuring the `OverrideDefaultSqliteModelCacheKeyFactory`.
    
    In `DbContextBuilderTestsBase.cs`, removed the `using` directive for `Microsoft.Extensions.DependencyInjection`, suggesting a shift away from dependency injection in tests. Commented out several test methods related to `UseServiceProvider`, indicating they need reworking.

--- src/Wolfgang.DbContextBuilder-Core/DbContextBuilder.cs
+++ src/Wolfgang.DbContextBuilder-Core/DbContextBuilder.cs
@@ -21,9 +21,9 @@ public class DbContextBuilder<T> where T : DbContext
 	}
     
 	private DbProvider _dbProvider = DbProvider.InMemory;
-    private readonly List<object> _seedData = new();
+    private readonly List<object> _seedData = [];
     //private IServiceProvider? _serviceProvider;
-    private readonly ServiceCollection _serviceCollection = new();
+    private readonly ServiceCollection _serviceCollection = [];
 
     private DbContextOptionsBuilder<T>? _dbContextOptionsBuilder;
 
@@ -109,66 +109,6 @@ public class DbContextBuilder<T> where T : DbContext
         return this;
     }
 
-
-
-    ///// <summary>
-    ///// Provides a method to override how the database is created since different databases
-    ///// engines support different features
-    ///// </summary>
-    ///// <param name="serviceProvider"></param>
-    ///// <returns><see cref="DbContextBuilder{T}"></see></returns>
-    ///// <remarks>
-    ///// This method allows you to intercept the creation of the database and alter or
-    ///// customize it. This is useful, for example, if your production database is SQL Server
-    ///// or Oracle and is using features that are not supported by the InMemory or Sqlite
-    ///// databases. Using this method and passing in your customizations, allows you to
-    ///// alter or opt out altogether certain columns.
-    /////
-    ///// Note: This does mean that the features that you change will not match your
-    ///// production system and so testing those features is pointless, however you can still
-    ///// test the rest of your code.
-    /////
-    ///// One such option is say your production database has a column that is a computed
-    ///// type using functions that is are not supported under InMemory or Sqlite database.
-    ///// You could use this method to alter the table to just store an integer rather than
-    ///// a calculation. As long as you seed the database correctly you can still use the
-    ///// table in your tests.
-    ///// </remarks>
-    //public DbContextBuilder<T> UseServiceProvider(IServiceProvider serviceProvider)
-    //{
-    //    ArgumentNullException.ThrowIfNull(serviceProvider);
-
-    //    _serviceProvider = serviceProvider;
-
-    //    return this;
-    //}
-
-
-
-    public DbContextBuilder<T> UseServices(IServiceCollection services)
-    {
-        throw new NotImplementedException("UseServices is not implemented yet");
-        //ArgumentNullException.ThrowIfNull(services);
-
-        //foreach (var service in services)
-        //{
-        //    switch (service.Lifetime)
-        //    {
-        //        case ServiceLifetime.Singleton:
-        //            _serviceCollection.AddSingleton(service.ServiceType, service.ImplementationInstance);
-        //            break;
-        //        case ServiceLifetime.Scoped:
-        //            _serviceCollection.AddScoped( service.ServiceType, service.ImplementationInstance);
-        //            break;
-        //        case ServiceLifetime.Transient:
-        //            _serviceCollection.AddTransient(service.ServiceType, service.ImplementationInstance);
-        //            break;
-        //        default:
-        //            throw new ArgumentOutOfRangeException();
-        //    }
-        //}
-        //return this;
-    }
 
 
     /// <summary>
@@ -384,23 +324,4 @@ public class DbContextBuilder<T> where T : DbContext
         // Create a new clean context instance to return
         return (T)Activator.CreateInstance(typeof(T), options)!;
     }
-
 }
-
-
-
-
-
-
-// TODO add property to config OverrideDefaultSqliteModelCacheKeyFactory = T/F
-// TODO Modify UseSqlite to return a SqliteDbContextBuilder with additional properties including this one
-
-//internal class SqliteModelCacheKeyFactory : IModelCacheKeyFactory
-//{
-//    public object Create(DbContext context, bool designTime) => new SqliteModelCacheKey(context, designTime);
-
-//    private sealed class SqliteModelCacheKey(DbContext context, bool designTime)
-//        : ModelCacheKey(context, designTime)
-//    {
-//    }
-//}
