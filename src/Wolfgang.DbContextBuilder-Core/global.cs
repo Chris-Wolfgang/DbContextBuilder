0b0aa8490fb73a6df740895fbd6bd0a9f9ef21b4
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-09-21 22:28:51 -0400

    Added // DevSkim: ignore DS176209 to all todo to suppress devskim warnings

--- src/Wolfgang.DbContextBuilder-Core/global.cs
+++ src/Wolfgang.DbContextBuilder-Core/global.cs
@@ -3,9 +3,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Wolfgang.DbContextBuilder-Core.Tests.Unit")]
 
 
-
-// TODO Create implementations for Bogus
-// TODO Allow user to configure Bogus
-// TODO Add tests
-//
-// TODO r/w a table in a schema other than default. Sqlite give warning about schema
+// TODO Create implementations for Bogus // DevSkim: ignore DS176209
+// TODO Allow user to configure Bogus // DevSkim: ignore DS176209
+// TODO Add tests // DevSkim: ignore DS176209
+// TODO r/w a table in a schema other than default. Sqlite give warning about schema// DevSkim: ignore DS176209
