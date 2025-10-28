7888eef18309c939df246af6286daf2d3e938575
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-10-27 21:30:24 -0400

    Moved each project for a specific version of EF to its own folder so that the builds for various version of EF don't interfere witth each other when building the solution. Each project then links to the main project's files so that there is only a single version of the files to update and maintain

--- src/Wolfgang.DbContextBuilder-Core/global.cs
+++ src/Wolfgang.DbContextBuilder-Core/global.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Wolfgang.DbContextBuilder-Core.Tests.Unit-EF6")]
 [assembly: InternalsVisibleTo("Wolfgang.DbContextBuilder-Core.Tests.Unit-EF7")]
 [assembly: InternalsVisibleTo("Wolfgang.DbContextBuilder-Core.Tests.Unit-EF8")]
+[assembly: InternalsVisibleTo("Wolfgang.DbContextBuilder-Core.Tests.Unit-EF9")]
 
 
 // TODO Create implementations for Bogus 
